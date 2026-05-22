51f2b90b16f285e235be19458eedbf688e47f33a
rickyandresen0 <rickyandresen0@gmail.com>
2026-05-22 13:35:26 +0700

    Add Health bar
    Add health bar, have not yet apply the damage

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -1,26 +1,46 @@
+using System.Data.Common;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-
     [SerializeField] private float startingHealth;
+    [SerializeField] private HealthBar healthBar;
     private float currentHealth;
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         currentHealth = startingHealth;
+        healthBar.SetMaxHealth((int)startingHealth);
     }
-    private void TakeDamage(float _damage)
-    {
-        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-        if (currentHealth > 0)
+    public void TakeDamage(float damage)
+    {   
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
+        healthBar.SetHealth((int)currentHealth);
+
+        if (currentHealth > 0) 
         {
-            //player alive
+            anim.SetTrigger("hurt");
         }
         else
         {
-            //player died
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+                Die();
+
+            }
         }
+    }
+
+    private void Die()
+    {
+
+        Debug.Log("Player died!");
     }
 }
