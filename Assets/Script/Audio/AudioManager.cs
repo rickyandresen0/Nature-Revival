96c6e1643645626479e52271820a132aab580c48
rickyandresen0 <rickyandresen0@gmail.com>
2026-06-02 20:46:02 +0700

    To add background music
    To add background music into the game

---
+++ Assets/Script/Audio/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip bakcground;
+    public AudioClip death;
+    public AudioClip shoot;
+    public AudioClip run;
+    public AudioClip takeDamage;
+    public AudioClip enemyTakeDamage;
+    public AudioClip enemyDie;
+    public AudioClip enemyShoot;
+
+    private void Start()
+    {
+        musicSource.clip = bakcground;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
