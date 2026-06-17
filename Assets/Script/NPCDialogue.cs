664de046187b2993e9bd703ec0c559e5203016ae
rickyandresen0 <rickyandresen0@gmail.com>
2026-06-18 00:28:11 +0700

    To add in NPC that is interactable
    To add in NPC for puzzle so we can go to the puzzle scene and do the puzzle

--- Assets/Script/NPCDialogue.cs
+++ Assets/Script/NPCDialogue.cs
@@ -1,16 +1,74 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NPCDialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private GameObject interactPrompt;
+    private bool playerInRange;
+
+    private void Update()
     {
-        
+      if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TryStartPuzzle();
+        }  
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryStartPuzzle()
     {
-        
+        //ngecek udh belum
+        int isCompleted = PlayerPrefs.GetInt("TrashPuzzleCompleted", 0);
+
+        if (isCompleted == 1)
+        {
+            ShowDialogue("Thank you for cleaning up!");
+            Invoke("HideDialogue", 2f);
+        }
+        else
+        {
+            ShowDialogue("Please help me!");
+            Invoke("LoadPuzzle", 1.5f);
+        }
+    }
+
+    private void ShowDialogue(string message)
+    {
+        dialogueText.text = message;
+        dialogueBox.SetActive(true);
+    }
+
+    private void HideDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+        private void LoadPuzzle()
+    {
+        HideDialogue();
+        SceneManager.LoadScene("Puzzle2");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+            if (interactPrompt != null)
+                interactPrompt.SetActive(true);            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            HideDialogue();
+            if (interactPrompt != null)
+                interactPrompt.SetActive(false);               
+        }
     }
 }
